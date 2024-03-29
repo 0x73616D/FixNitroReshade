af9d7bd2361faa2d34aa4a49d474b616df6b620f
Cabezon.luac <94794277+0x73616D@users.noreply.github.com>
2024-03-29 16:25:39 -0300

    NoMoreHazeOnNitroReshadeAndEnbFix.cpp

---
+++ NoMoreHazeOnNitroReshadeAndEnbFix.cpp
@@ -0,0 +1,7 @@
+class Script {
+public:
+    std::string NickName = "NoMoreHazeOnNitroReshadeAndEnbFix.ASI";
+    std::vector<std::string> By = {"0x73616D.luac"};
+    std::vector<std::string> Code = {"C++"};
+    std::string BuyThisScript = "https://discord.com/users/717764929113030756";
+};
